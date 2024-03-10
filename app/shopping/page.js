9405a478221ef0f0c2062446c082728e00237adf
Graph and Co <contact@graphandco.com>
2024-03-10 21:06:07 +0100

    Changed shopping list style

--- app/shopping/page.js
+++ app/shopping/page.js
@@ -36,24 +36,24 @@ const Home = async () => {
 		<main className="flex min-h-screen flex-col items-center justify-between">
 			<div className="container mt-5">
 				<h1 className="font-title text-4xl mb-4 font-black text-foreground">
-					Lucelle App -
+					Courses
 				</h1>
-				<div className="grid grid-cols-3 gap-3">
+				<div className="grid grid-cols-4 gap-3">
 					{images.map((image) => (
-						<Card
+						<div
 							key={image.public_id}
-							className="flex flex-col items-center justify-center p-2"
+							className="flex flex-col items-center justify-center p-1 border border-foreground/20 rounded-2xl"
 						>
 							<img
 								src={image.secure_url}
 								alt={image.public_id}
 								className="h-8"
 							/>
-							<div className="text-center text-xs mt-2 capitalize italic pointer-events-none">
+							<div className="text-center text-xs break-all mt-1 capitalize pointer-events-none">
 								{cleanID(image.public_id)}
 							</div>
 							{/* <div>{image.public_id}</div> */}
-						</Card>
+						</div>
 					))}
 				</div>
 			</div>
